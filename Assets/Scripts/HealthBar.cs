f628039bdc4b5650c054f5c093d850e83d243e72
Moti Baral <animamaster999@gmail.com>
2023-07-08 11:35:26 +0300

    health bar

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,30 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private Slider _slider;
+    [SerializeField] private TextMeshProUGUI _text;
+    [SerializeField] private bool _showMaxHealthText;
+
+    public void SetMaxHealth(int maxHealth)
+    {
+        _slider.maxValue = maxHealth;
+        _slider.value = maxHealth;
+    }
+
+    public void SetHealth(int health)
+    {
+        _slider.value = health;
+
+        if (_showMaxHealthText)
+        {
+            _text.text = $"{health} / {_slider.maxValue}";
+
+            return;
+        }
+
+        _text.text = health.ToString();
+    }
+}
