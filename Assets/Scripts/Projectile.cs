0667708e03978e7a70b7f96a0e4872eb9cd41587
boazlesh <boazlesh@gmail.com>
2023-07-09 16:48:46 +0300

    plead projectile

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -14,6 +14,7 @@ namespace Assets.Scripts
         private Vector3 _sourcePosition;
         private Vector3 _targetPosition;
         private bool _shouldFlip;
+        private float _originalAlpha;
 
         public IEnumerator ProjectAbilityRoutine(Unit source, Unit target, Ability ability)
         {
@@ -21,9 +22,10 @@ namespace Assets.Scripts
             _spriteRenderer.sprite = ability.ProjectileSprite;
             _startTime = DateTime.UtcNow;
 
-            _sourcePosition = (!ability.UseCustomSource ? source.transform.position : (target.transform.position + ability.CustomSourcePointRelativeToTarget));
+            _sourcePosition = (!ability.UseCustomSource ? source.transform.position : (ability.CustomSourcePointRelativeToTarget.sqrMagnitude == 0 ? (source.transform.position + ability.CustomSourcePointRelativeToSource) : (target.transform.position + ability.CustomSourcePointRelativeToTarget)));
             _targetPosition = target.transform.position;
             _shouldFlip = !source.IsPlayerUnit && !ability.DontFlipForEnemy;
+            _originalAlpha = _spriteRenderer.color.a;
 
             yield return new WaitUntil(() => _didReachDestination);
 
@@ -44,6 +46,11 @@ namespace Assets.Scripts
             transform.position = Vector3.Lerp(_sourcePosition, _targetPosition, _ability.ProjectileCurve.Evaluate(lerpAlpha));
             transform.rotation = Quaternion.Euler(Vector3.Lerp(_ability.SourceRotation, _ability.TargetRotation, _ability.ProjectileRotationCurve.Evaluate(lerpAlpha)) + new Vector3(0, 0, _shouldFlip ? 180 : 0));
             transform.localScale = Vector3.Lerp(_ability.SourceScale, _ability.TargetScale, _ability.ProjectileScaleCurve.Evaluate(lerpAlpha));
+
+            if (_ability.CustomFade)
+            {
+                _spriteRenderer.color = new Color(_spriteRenderer.color.r, _spriteRenderer.color.g, _spriteRenderer.color.b, _originalAlpha * _ability.ProjectileFadeCurve.Evaluate(lerpAlpha));
+            }
         }
     }
 }
